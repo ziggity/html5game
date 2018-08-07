5972fad136a74646f090443a169632577e5436c6
zach beecher <zachbeecher@gmail.com>
2018-08-06 22:56:31 -0700

    add Player prototype and object

--- game.js
+++ game.js
@@ -34,7 +34,54 @@ function begin() {
     requestAnimationFrame(loop);
     console.log((imgSprite, 0, 0, canvasWidth, canvasHeight, 0, 0, canvasWidth, canvasHeight, 0,0))
 }
-function loop() {
-
+function update(){
+    clearCtx(ctxEntities);
+    //updateAllEnemies();
+    //player1.update();
 }
+function loop() {
+    if (isPlaying) {
+        console.log('loop')
+        update();
+        draw();
+        requestAnimFrame(loop);
+    }
+}
+
+function clearCtx(ctx) {
+    ctx.clearRect(0,0,canvasWidth, canvasHeight);
+}
+function randomRange(min,max){
+    return Math.floor(Math.random() * (max+1-min)) +min;
+}
+
+function Player (){
+    this.srcX = 0;
+    this.srcY = 600;
+    this.width = 35;
+    this.height = 54;
+    this.drawX = 400;
+    this.drawY = 300;
+    this.centerX = this.drawX + (this.width/2);
+    this.centerY = this.drawY + (this.height /2);
+    this.speed = 2;
+    // this.isUpKey = false;
+    // this.isRightKey = false;
+    // this.isDownKey = false;
+    // this.isLeftKey = false;
+    // this.isSpacebar = false;
+    // this.isShooting = false;
+    // var numBullets = 10;
+    // this.bullets = [];
+    // this.currentBullet = 0;
+    // for(let i=0; i<numBullets; i++){
+    //     this.bullets[this.bullets.length] = new Bullet();
+    // }
+}
+Player.prototype.update() = function () {
+    this.centerX = this.drawX + (this.width /2);
+    this.centerY = this.drawY + (this.height /2);
+    //this.checkDirection();
+}
+
 
